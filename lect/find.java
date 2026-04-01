76f34247c6707e9a80ffe5dfc516d33e519534ca
Anurag1707 <Anuragjhariya1707@gmail.com>
2026-04-01 13:11:08 +0530

    find

---
+++ lect/find.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class find {
+     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] arr= new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i]=sc.nextInt();
+        }
+       int d= sc.nextInt();
+       int index=-1;
+       for (int i = 0; i < n ; i++) {
+        if(arr[i] == d){
+            index= i;
+            break;
+        }
+       }
+       System.out.println("in index" + index);
+
+    }
+}
